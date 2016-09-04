f1b7eca28d9a9f2890c429b1d33d7506562a476a
melevir <melevir@gmail.com>
2016-09-04 10:20:44 +0300

    added boilerplate

---
+++ bars.py
@@ -0,0 +1,21 @@
+import json
+
+
+def load_data(filepath):
+    pass
+
+
+def get_biggest_bar(data):
+    pass
+
+
+def get_smallest_bar(data):
+    pass
+
+
+def get_closest_bar(data, longitude, latitude):
+    pass
+
+
+if __name__ == '__main__':
+    pass
