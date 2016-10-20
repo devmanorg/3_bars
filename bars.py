85f63592e8fac2c60e26865e1326628f896596ee
Alexey <menyalin@icloud.com>
2016-10-20 20:46:59 +0300

    1

--- bars.py
+++ bars.py
@@ -2,11 +2,30 @@ import json
 
 
 def load_data(filepath):
-    pass
+    my_file = open(filepath, mode="r", encoding='utf-8')
+    json_data = json.load(my_file)
+    my_file.close()
+    return json_data
 
 
 def get_biggest_bar(data):
-    pass
+    bar_list = ()
+    max_seats_count = 0
+    for bar in data:
+        if bar['Cells']['SeatsCount'] > max_seats_count:
+            max_seats_count = bar['Cells']['SeatsCount']
+    for bar in json_data:
+        if bar['Cells']['SeatsCount'] == max_seats_count:
+            bar_list.append(bar['Cells']['Name'])
+    if bar_list.count() > 1:
+        print("---------------------------------------")
+        print()
+
+
+        for x in bar_list:
+            print(x)
+    elif bar_list.count() <= 0:
+        print("Больших баров не найдено!")
 
 
 def get_smallest_bar(data):
@@ -18,4 +37,4 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    json_data = load_data('Бары.json')
