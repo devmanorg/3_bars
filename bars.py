a13af4606108af6798badb9adadbd20fbfefe113
menyalin <menyalin@gmail.com>
2016-10-23 10:49:39 +0300

    biggest+smallest bars

--- bars.py
+++ bars.py
@@ -9,27 +9,45 @@ def load_data(filepath):
 
 
 def get_biggest_bar(data):
-    bar_list = ()
     max_seats_count = 0
+    bar_list = []
     for bar in data:
         if bar['Cells']['SeatsCount'] > max_seats_count:
             max_seats_count = bar['Cells']['SeatsCount']
     for bar in json_data:
         if bar['Cells']['SeatsCount'] == max_seats_count:
             bar_list.append(bar['Cells']['Name'])
-    if bar_list.count() > 1:
-        print("---------------------------------------")
-        print()
-
-
+    if len(bar_list) >= 2:
+        print('Самые большие бары Москвы: ')
         for x in bar_list:
-            print(x)
-    elif bar_list.count() <= 0:
+            print('   ' + x)
+    elif len(bar_list) <= 0:
         print("Больших баров не найдено!")
+    else:
+        print('Самый большой бар в Москве: ' + bar_list[0])
 
 
 def get_smallest_bar(data):
-    pass
+    max_seats_count = 0
+    bar_list = []
+    for bar in data:
+        if bar['Cells']['SeatsCount'] > max_seats_count:
+            max_seats_count = bar['Cells']['SeatsCount']
+    min_seats_count = max_seats_count
+    for bar in data:
+        if bar['Cells']['SeatsCount'] < min_seats_count:
+            min_seats_count = bar['Cells']['SeatsCount']
+    for bar in json_data:
+        if bar['Cells']['SeatsCount'] == min_seats_count:
+            bar_list.append(bar['Cells']['Name'])
+    if len(bar_list) >= 2:
+        print('Самые маленькие бары Москвы: ')
+        for x in bar_list:
+            print('   ' + x)
+    elif len(bar_list) <= 0:
+        print("Баров не найдено!")
+    else:
+        print('Самый маленький бар в Москве: ' + bar_list[0])
 
 
 def get_closest_bar(data, longitude, latitude):
@@ -38,3 +56,5 @@ def get_closest_bar(data, longitude, latitude):
 
 if __name__ == '__main__':
     json_data = load_data('Бары.json')
+    get_biggest_bar(json_data)
+    get_smallest_bar(json_data)
