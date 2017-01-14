97c34f209bedfc38e439bdd7cdf66cd540857632
DanGalkin <dangalkin@gmail.com>
2017-01-15 01:31:47 +0300

    Update bars.py
    Deleted string with encoding
    Renamed some variables

--- bars.py
+++ bars.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 import json
 
 
 def load_data(filepath):
     with open(filepath, encoding='utf-8') as data_file:
-        data = json.loads(data_file.read())
-    return data
+        bar_data = json.loads(data_file.read())
+    return bar_data
 
 
 def get_biggest_bar(data):
@@ -32,17 +31,17 @@ def get_smallest_bar(data):
 
 
 def get_closest_bar(data, longitude, latitude):
-    bar_la = data[1]['geoData']['coordinates'][1]
-    bar_lo = data[1]['geoData']['coordinates'][0]
-    shortest_dist = (bar_la - latitude) ** 2 + (bar_lo - longitude) ** 2
+    bar_latitude = data[1]['geoData']['coordinates'][1]
+    bar_longitude = data[1]['geoData']['coordinates'][0]
+    shortest_distance = (bar_latitude - latitude) ** 2 + (bar_longitude - longitude) ** 2
     for bar in data:
-        bar_la = bar['geoData']['coordinates'][1]
-        bar_lo = bar['geoData']['coordinates'][0]
-        dist = (bar_la - latitude) ** 2 + (bar_lo - longitude) ** 2
-        if dist < shortest_dist:
+        bar_latitude = bar['geoData']['coordinates'][1]
+        bar_longitude = bar['geoData']['coordinates'][0]
+        distance = (bar_latitude - latitude) ** 2 + (bar_longitude - longitude) ** 2
+        if distance < shortest_distance:
             nearest_bar = bar['Name']
             nearest_bar_address = bar['Address']
-            shortest_dist = dist
+            shortest_distance = distance
     print('Ближайший бар: ', nearest_bar)
     print(nearest_bar_address)
 
@@ -52,13 +51,13 @@ if __name__ == '__main__':
     print('Где лежит таблица с файлами? Укажи путь:')
     filepath = str(input())
     # open data in json
-    data = load_data(filepath)
-    get_biggest_bar(data)
-    get_smallest_bar(data)
+    bar_data = load_data(filepath)
+    get_biggest_bar(bar_data)
+    get_smallest_bar(bar_data)
     # getting longitude
     print('А теперь давай узнаем, какой бар ближе. Введи свои координаты.')
     print('Узнай в гугле, на какой широте ты находишься:')
     latitude = float(input())
     print('Отично, теперь узнай в гугле, на какой долготе ты находишься:')
     longitude = float(input())
-    get_closest_bar(data, longitude, latitude)
+    get_closest_bar(bar_data, longitude, latitude)
