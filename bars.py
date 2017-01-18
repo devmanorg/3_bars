b8761a36319e687dc720f8deb5fe2078d6593a1f
Frodan <daniil.fronc@mail.ru>
2017-01-18 12:47:49 +0700

    Полностью переписаны функции
    Теперь функции используют min/max вместо обхода по циклу.

--- bars.py
+++ bars.py
@@ -8,38 +8,22 @@ def load_data(filepath):
 
 
 def get_biggest_bar(bars_json):
-    max_seats = 0
-    for bar in bars_json:
-        if bar['SeatsCount'] > max_seats:
-            max_seats = bar['SeatsCount']
-            biggest_bar = bar
-    return "Biggest bar is " + biggest_bar['Name']
-
+    return max(bars_json, key=lambda x: x['SeatsCount'])
 
 def get_smallest_bar(bars_json):
-    min_seats = bars_json[0]['SeatsCount']
-    for bar in bars_json:
-        if bar['SeatsCount'] < min_seats:
-            min_seats = bar['SeatsCount']
-            smallest_bar = bar       
-    return 'Smallest bar is ' + smallest_bar['Name']
+    return min(bars_json, key=lambda x: x['SeatsCount'])
 
 def get_closest_bar(bars_json, longitude, latitude):
-    coordinates = bars_json[0]['geoData']['coordinates']
-    length = ((coordinates[0] - longitude)**2 + (coordinates[1] - latitude)**2)**0.5
-    for bar in bars_json:
-        coordinates = bar['geoData']['coordinates']
-        tmp = ((coordinates[0] - longitude)**2 + (coordinates[1] - latitude)**2)**0.5
-        if tmp < length:
-            closest_bar = bar
-            length = tmp
-    return "Closest bar is " + closest_bar['Name']
+    return min(bars_json, key=lambda x: ((x['geoData']['coordinates'][0] - longitude)**2 +
+                                         (x['geoData']['coordinates'][1] - latitude)**2)**0.5)
 
 if __name__ == '__main__':
     bars_json = load_data(input('Путь:\n'))
     longitude = float(input("Longitude:\n"))
     latitude = float(input("Latitude:\n"))
-    print(get_biggest_bar(bars_json))
-    print(get_smallest_bar(bars_json))
-    print(get_closest_bar(bars_json, longitude, latitude))
+    biggest_bar = get_biggest_bar(bars_json)['Name']
+    smallest_bar = get_smallest_bar(bars_json)['Name']
+    closest_bar = get_closest_bar(bars_json, longitude, latitude)['Name']
+    print('The biggest bar is {0}, the smallest is {1}.'.format(biggest_bar,smallest_bar))
+    print("Closest bar is {0}".format(closest_bar))
     
