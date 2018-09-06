ad9eca099499798e643149300009f266d9c92b71
Krendelev <krendelev@gmail.com>
2018-09-06 23:47:22 +0300

    minor refactoring

--- bars.py
+++ bars.py
@@ -7,29 +7,29 @@ def load_data(filepath):
         return json.load(file_handler)['features']
 
 
-def get_biggest_bar(data, *args):
-    biggest_bar = max(data,
+def get_biggest_bar(bar_data, *args):
+    biggest_bar = max(bar_data,
                       key=lambda x: x['properties']['Attributes']['SeatsCount']
                       )
     return biggest_bar
 
 
-def get_smallest_bar(data, *args):
-    smallest_bar = min(data,
+def get_smallest_bar(bar_data, *args):
+    smallest_bar = min(bar_data,
                        key=lambda x: x['properties']['Attributes']['SeatsCount']
                        )
     return smallest_bar
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(bar_data, longitude, latitude):
 
-    def get_distance(data, longitude, latitude):
-        longitude2, latitude2 = data['geometry']['coordinates']
+    def get_distance(bar_data, longitude, latitude):
+        longitude2, latitude2 = bar_data['geometry']['coordinates']
         distance = ((longitude2 - longitude) ** 2 +
                     (latitude2 - latitude) ** 2) ** 0.5
         return distance
 
-    closest_bar = min(data, key=lambda x: get_distance(x, longitude, latitude))
+    closest_bar = min(bar_data, key=lambda x: get_distance(x, longitude, latitude))
     return closest_bar
 
 
@@ -44,17 +44,16 @@ if __name__ == '__main__':
         print('File not found')
     except ValueError:
         print('Not a valid JSON file')
-    else:
-        feature_list = {
-            'большой': get_biggest_bar,
-            'маленький': get_smallest_bar,
-            'близкий': get_closest_bar
-        }
-        for adjective, feature in feature_list.items():
-            bar_feature = feature(bar_list, longitude, latitude)
-            print(
-                "Самый {0} бар – {1},"
-                .format(adjective, bar_feature['properties']['Attributes']['Name']),
-                "Мест: {},".format(bar_feature['properties']['Attributes']['SeatsCount']),
-                "Адрес: {}".format(bar_feature['properties']['Attributes']['Address'])
-            )
+    feature_list = {
+        'большой': get_biggest_bar,
+        'маленький': get_smallest_bar,
+        'близкий': get_closest_bar
+    }
+    for adjective, feature in feature_list.items():
+        bar_feature = feature(bar_list, longitude, latitude)
+        print(
+            "Самый {0} бар – {1},"
+            .format(adjective, bar_feature['properties']['Attributes']['Name']),
+            "Мест: {},".format(bar_feature['properties']['Attributes']['SeatsCount']),
+            "Адрес: {}".format(bar_feature['properties']['Attributes']['Address'])
+        )
