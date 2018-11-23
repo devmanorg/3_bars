d60ec5a2a689444c93b09c65329d8311cae47ab9
Igor <nekto.korobko@gmail.com>
2018-11-24 02:16:15 +0300

    Complete

--- bars.py
+++ bars.py
@@ -2,20 +2,38 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_json:
+        json_loaded = json.load(file_json)
+    return json_loaded
 
 
 def get_biggest_bar(data):
-    pass
+    return max(data["features"], key=lambda feature:
+    feature["properties"]["Attributes"]["SeatsCount"])
 
 
 def get_smallest_bar(data):
-    pass
+    return min(data["features"], key=lambda feature:
+    feature["properties"]["Attributes"]["SeatsCount"])
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    return min(data["features"], key=lambda feature:
+    get_distance_of_two_points(feature["geometry"]["coordinates"], [longitude, latitude]))
+
+
+def get_distance_of_two_points(bar_coordinate, your_coordinate):
+    return (bar_coordinate[0] - your_coordinate[0]) ** 2 + (bar_coordinate[1] - your_coordinate[1]) ** 2
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(input('Укажите путь к файлу: '))
+    longitude = float(input("Введите долготу: "))
+    latitude = float(input("Введите широту: "))
+    biggest_bar = get_biggest_bar(data)
+    smallest_bar = get_smallest_bar(data)
+    closest_bar = get_closest_bar(data, longitude, latitude)
+    print("Самый большой бар: " + biggest_bar["properties"]["Attributes"]["Name"])
+    print("Самый маленький бар: " + smallest_bar["properties"]["Attributes"]["Name"])
+    print("Самый ближайший бар: " + closest_bar["properties"]["Attributes"]["Name"] + '. Находиться по адресу: ' +
+          closest_bar["properties"]["Attributes"]["Address"])
