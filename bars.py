8b89bc483e47b9f21dd79e9ce6dfcda16fb7c5e7
nekto007 <nekto.korobko@gmail.com>
2018-12-02 19:44:33 +0300

    Update bars.py
    Сделано единообразие кавычек,
    Исправлены опечатки, переименованы фунции и причесал код.

--- bars.py
+++ bars.py
@@ -3,13 +3,13 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath, "r") as file_json:
+    with open(filepath, "r", encoding="utf-8") as file_json:
         json_loaded = json.load(file_json)
     return json_loaded
 
 
 def get_coordinates(bar):
-    return bar['geometry']['coordinates']
+    return bar["geometry"]["coordinates"]
 
 
 def get_seats_count(bar):
@@ -17,31 +17,36 @@ def get_seats_count(bar):
 
 
 def get_name(bar):
-    return bar['properties']['Attributes']['Name']
+    return bar["properties"]["Attributes"]["Name"]
 
 
 def get_address(bar):
     return bar["properties"]["Attributes"]["Address"]
 
 
-def get_biggest_bar(bar):
-    return max(bar, key=get_seats_count)
+def get_biggest_bar(bars):
+    return max(bars, key=get_seats_count)
 
 
-def get_smallest_bar(bar):
-    return min(bar, key=get_seats_count)
+def get_smallest_bar(bars):
+    return min(bars, key=get_seats_count)
 
 
 def get_closest_bar(bars, your_coordinate):
     return min(bars, key=lambda
-        bar: get_distance_of_two_points(get_coordinates(bar), your_coordinate))
+        bar: get_distance_between_two_points(
+        get_coordinates(bar), your_coordinate))
 
 
-def get_distance_of_two_points(bar_coordinate, your_coordinate):
-    return (bar_coordinate[0] - your_coordinate[0]) ** 2 + (bar_coordinate[1] - your_coordinate[1]) ** 2
+def get_distance_between_two_points(bar_coordinate, your_coordinate):
+    try:
+        return (bar_coordinate[0] - your_coordinate[0]) ** 2 + (
+                bar_coordinate[1] - your_coordinate[1]) ** 2
+    except TypeError:
+        print("Координаты введены некорректно")
 
 
-def input_coorditane():
+def get_user_coordinates():
     try:
         longitude = float(input("Введите долготу: "))
         latitude = float(input("Введите широту: "))
@@ -50,20 +55,20 @@ def input_coorditane():
         print("Координаты введены некорректно")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        bars = load_data(sys.argv[1])['features']
+        bars = load_data(sys.argv[1])["features"]
     except (FileNotFoundError, IndexError):
         print("Некоректно указан путь к файлу или файл не существует")
     except json.decoder.JSONDecodeError:
         print("Не корректное содержимое JSON файла")
-    your_coordinate = input_coorditane()
-
-    selected_bars = {
-        'Самый большой бар:': get_biggest_bar(bars),
-        'Самый маленький бар:': get_smallest_bar(bars),
-        'Самый ближайший бар:': get_closest_bar(bars, your_coordinate)
-    }
-    for title, bar in selected_bars.items():
-        print("{} {}. Находиться по адресу: {} и может вмещать посетителей: {}".format(
-            title, get_name(bar), get_address(bar), get_seats_count(bar)))
+    your_coordinate = get_user_coordinates()
+    if your_coordinate is not None:
+        selected_bars = {
+            "Самый большой бар:": get_biggest_bar(bars),
+            "Самый маленький бар:": get_smallest_bar(bars),
+            "Самый ближайший бар:": get_closest_bar(bars, your_coordinate)
+        }
+        for title, bar in selected_bars.items():
+            print("{} {}. Находиться по адресу: {} и может вмещать посетителей: {}".format(
+                title, get_name(bar), get_address(bar), get_seats_count(bar)))
